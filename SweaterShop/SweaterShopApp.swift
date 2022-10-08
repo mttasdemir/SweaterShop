a3de47171f46104cc2695cedde77c9acfd9de6fd
mttasdemir <mt.tasdemir@gmail.com>
2022-10-08 20:01:47 +0300

    Initial Commit

---
+++ SweaterShop/SweaterShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopApp.swift
+//  SweaterShop
+//
+//  Created by Mustafa Taşdemir on 8.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
